684a35057b86a57a0306ce73be4902e14863fe68
itliao <liao.irene.t@gmail.com>
2023-11-08 20:07:02 -0800

    Create CLfinder_summary5.sh
    script for last step of CLfinder workflow - generate a summary output

---
+++ Orthogroup/OrthNet/Step2_CLFinder/CLfinder_summary5.sh
@@ -0,0 +1,18 @@
+###############
+# diploid set #
+###############
+
+/path_to_OrthNet_scripts/create_CLfm_summary.py \
+	/path_to_list/230927_diploid.list \
+	230927_CLfinder_summary.txt \
+	-p /path_to_output/230927_diploid_out.1
+	
+#####################
+# higher ploidy set #
+#####################
+
+# 230214
+/path_to_OrthNet_scripts/create_CLfm_summary.py \
+	/path_to_list/230214_full.list \
+	230214_CLfinder_summary.txt \
+	-p /path_to_output/230914_full_out.1
