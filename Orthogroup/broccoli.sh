e49298a505b32e67c4ab25e1c0e3793b904e6110
itliao <liao.irene.t@gmail.com>
2023-11-08 18:43:58 -0800

    Create broccoli.sh
    scripts to run broccoli

---
+++ Orthogroup/broccoli.sh
@@ -0,0 +1,9 @@
+echo "broccoli on brassicaceae-diploid species"
+python /path_to_broccoli/broccoli.py \
+	-dir /path_to_proteomes/dip_broccoli \
+	-threads 8
+	
+echo "broccoli on brassicaceae-full set"
+python /path_to_broccoli/broccoli.py \
+	-dir /path_to_proteomes/full_BR \
+	-threads 8
