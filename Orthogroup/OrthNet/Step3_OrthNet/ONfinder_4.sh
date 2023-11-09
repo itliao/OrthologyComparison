a5f0f884c02ddf73dc9c3ff91f5b41b8a3eea2bb
itliao <liao.irene.t@gmail.com>
2023-11-08 20:21:52 -0800

    Create ONfinder_4.sh
    scripts to create output for cytoscape

---
+++ Orthogroup/OrthNet/Step3_OrthNet/ONfinder_4.sh
@@ -0,0 +1,13 @@
+###############
+# diploid set #
+###############
+
+/path_to_OrthNet_scripts/format_OrthNetEdges_4SIF.py \
+	/path_to_files/230927_pairs/ONfinder/230927_diploid.2/230927_diploid.clstrd.afterMCL.edges
+	
+#####################
+# higher ploidy set #
+#####################
+
+/path_to_OrthNet_scripts/format_OrthNetEdges_4SIF.py \
+	/path_to_files/230214_pairs/ONfinder/230214_full.2/230214_full.clstrd.afterMCL.edges
