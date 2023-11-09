ec67862bbeb65eed71e1993b7162ea127dd6efce
itliao <liao.irene.t@gmail.com>
2023-11-08 20:25:39 -0800

    Create ONfinder_5.sh
    script for summary output from OrthNet

---
+++ Orthogroup/OrthNet/Step3_OrthNet/ONfinder_5.sh
@@ -0,0 +1,17 @@
+###############
+# diploid set #
+###############
+
+/path_to_OrthNet_scripts/create_CLfm_summary.py \
+	230927_diploid \
+	/path_to_files/230927_CLfinder_summary.afterOrthNet.txt \
+	-p /path_to_output/230927_pairs/ONfinder/230927_diploid.2
+	
+#####################
+# higher ploidy set #
+#####################
+
+/path_to_OrthNet_scripts/create_CLfm_summary.py \
+	230214_full \
+	/path_to_files/230915_CLfinder_summary.afterOrthNet.txt \
+	-p /path_to_output/230214_pairs/ONfinder/230214_full.2
